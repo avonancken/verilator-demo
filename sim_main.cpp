e144065cd84c77194361cfe4d645552bda00655d
Adam Von Ancken <avonancken@availink.com>
2024-05-23 19:39:13 -0400

    add vcd dump

--- sim_main.cpp
+++ sim_main.cpp
@@ -2,17 +2,29 @@
 
 #include "Vour.h"
 #include "verilated.h"
+#include "verilated_vcd_c.h"
+
+vluint64_t sim_time = 0;
 int main(int argc, char **argv, char **env)
 {
         Verilated::commandArgs(argc, argv);
+	Verilated::traceEverOn(true);
         Vour *top = new Vour;
-        while (!Verilated::gotFinish())
+	VerilatedVcdC *m_trace = new VerilatedVcdC;
+	top->trace(m_trace, 5);
+	m_trace->open("waveform.vcd");
+	int count = 0;  
+        while (!Verilated::gotFinish() && (count < 1000))
         {
                 if (top->clock)
                         std::cout << " " << top->fn << std::endl;
                 top->clock ^= 1;
                 top->eval();
+		m_trace->dump(sim_time);
+		sim_time++;
+		count++;
         }
+	m_trace->close();
         delete top;
         exit(0);
 }
